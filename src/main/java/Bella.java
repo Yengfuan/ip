8d5eb863c09a836f4c530c1ba664bd8c0f3a948a
yengfuan <fengyuan3426@gmail.com>
2026-01-29 17:06:35 +0800

    Add echo functionality

--- src/main/java/Bella.java
+++ src/main/java/Bella.java
@@ -1,12 +1,29 @@
+import java.util.Scanner;
+
 public class Bella {
-    public static void main(String[] args) {
+    private static final String DIVIDER = "____________________________________________________________\n";
+    public static void greet() {
         String logo = "   __\n" +
                 "o-''|\\_____/)\n" +
                 " \\_/|_)     )\n" +
                 "    \\  __  /\n" +
                 "    (_/ (_/";
-        System.out.println("Hello I'm Bella, your personal chatbot!\n" + logo);
-        System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(DIVIDER + "Howl you doing I'm Bella, your personal chatbot!\n" + logo);
+    }
+
+    public static void echo() {
+        String line;
+        Scanner in = new Scanner(System.in);
+        System.out.println("What can I do for you?\n" + DIVIDER);
+        line = in.nextLine();
+        while (!line.equals("bye")) {
+            System.out.println(DIVIDER + "Woof " + line + " woof\n" + DIVIDER);
+            line = in.nextLine();
+        }
+    }
+    public static void main(String[] args) {
+        greet();
+        echo();
+        System.out.println(DIVIDER + "Leaving the paw-ty? See you later!\n" + DIVIDER);
     }
 }
