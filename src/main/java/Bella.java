7343a1fec793860b6dd4a8a9a98a3acaaa4387a4
yengfuan <fengyuan3426@gmail.com>
2026-01-29 17:42:17 +0800

    Add task listing and storage functionality

--- src/main/java/Bella.java
+++ src/main/java/Bella.java
@@ -11,19 +11,46 @@ public class Bella {
         System.out.println(DIVIDER + "Howl you doing I'm Bella, your personal chatbot!\n" + logo);
     }
 
-    public static void echo() {
+    public static void listTasks(String[] taskList, int taskCount) {
+        if (taskCount != 0) {
+            System.out.println(DIVIDER);
+            for (int i = 0; i < taskCount; i++) {
+                System.out.println((i + 1) + ". " + taskList[i]);
+            }
+            System.out.println(DIVIDER);
+        } else {
+            System.out.println(DIVIDER + "You haven't added anything yet! Woof!\n" + DIVIDER);
+        }
+    }
+
+    public static void manageTask() {
         String line;
+        String[] taskList = new String[100];
+        int taskCount = 0;
         Scanner in = new Scanner(System.in);
+
         System.out.println("What can I do for you?\n" + DIVIDER);
+
         line = in.nextLine();
         while (!line.equals("bye")) {
-            System.out.println(DIVIDER + "Woof " + line + " woof\n" + DIVIDER);
+            String command = line;
+
+            switch (command) {
+            case "list":
+                listTasks(taskList, taskCount);
+                break;
+            default:
+                taskList[taskCount] = line;
+                taskCount++;
+                System.out.println(DIVIDER + "I've added: " + line + "\n" + DIVIDER);
+            }
+
             line = in.nextLine();
         }
     }
     public static void main(String[] args) {
         greet();
-        echo();
+        manageTask();
         System.out.println(DIVIDER + "Leaving the paw-ty? See you later!\n" + DIVIDER);
     }
 }
