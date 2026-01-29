039c7b26302a49bd7120c1cbfce8bc9afab6c699
yengfuan <fengyuan3426@gmail.com>
2026-01-29 14:36:49 +0800

    Add Increment: Level-0

---
+++ src/main/java/Bella.java
@@ -0,0 +1,12 @@
+public class Bella {
+    public static void main(String[] args) {
+        String logo = "   __\n" +
+                "o-''|\\_____/)\n" +
+                " \\_/|_)     )\n" +
+                "    \\  __  /\n" +
+                "    (_/ (_/";
+        System.out.println("Hello I'm Bella, your personal chatbot!\n" + logo);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
