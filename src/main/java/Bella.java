eb37284f77399c28c78829146c65992b14f8c1e7
yengfuan <fengyuan3426@gmail.com>
2026-01-29 19:28:21 +0800

    Add mark as done functionality

--- src/main/java/Bella.java
+++ src/main/java/Bella.java
@@ -11,11 +11,11 @@ public class Bella {
         System.out.println(DIVIDER + "Howl you doing I'm Bella, your personal chatbot!\n" + logo);
     }
 
-    public static void listTasks(String[] taskList, int taskCount) {
+    public static void listTasks(Task[] taskList, int taskCount) {
         if (taskCount != 0) {
             System.out.println(DIVIDER);
             for (int i = 0; i < taskCount; i++) {
-                System.out.println((i + 1) + ". " + taskList[i]);
+                System.out.println((i + 1) + ". " + taskList[i].description + " [" + taskList[i].getStatusIcon() + "]");
             }
             System.out.println(DIVIDER);
         } else {
@@ -24,23 +24,38 @@ public class Bella {
     }
 
     public static void manageTask() {
-        String line;
-        String[] taskList = new String[100];
+        Task[] taskList = new Task[100];
         int taskCount = 0;
         Scanner in = new Scanner(System.in);
 
         System.out.println("What can I do for you?\n" + DIVIDER);
 
-        line = in.nextLine();
+        String line = in.nextLine();
         while (!line.equals("bye")) {
-            String command = line;
+            String[] parts = line.split(" ", 2);
+            String command = parts[0];
 
             switch (command) {
             case "list":
+                System.out.println(DIVIDER + "Here are the tasks in your list:\n");
                 listTasks(taskList, taskCount);
                 break;
+            case "mark":
+                if (parts.length > 1) {
+                    handleMarkTask(taskList, taskCount, parts[1], true);
+                } else {
+                    System.out.println(DIVIDER + "Please specify a task number to mark\n" + DIVIDER);
+                }
+                break;
+            case "unmark":
+                if (parts.length > 1) {
+                    handleMarkTask(taskList, taskCount, parts[1], false);
+                } else {
+                    System.out.println(DIVIDER + "Please specify a task number to mark\n" + DIVIDER);
+                }
+                break;
             default:
-                taskList[taskCount] = line;
+                taskList[taskCount] = new Task(line);
                 taskCount++;
                 System.out.println(DIVIDER + "I've added: " + line + "\n" + DIVIDER);
             }
@@ -48,6 +63,33 @@ public class Bella {
             line = in.nextLine();
         }
     }
+
+    private static void handleMarkTask(Task[] taskList, int taskCount, String taskNumStr, boolean isDone) {
+        try {
+            int taskNum = Integer.parseInt(taskNumStr);
+
+            if (taskNum < 1 || taskNum > taskCount) {
+                System.out.println(DIVIDER + "Task number out of range!\n" + DIVIDER);
+                return;
+            }
+
+            Task task = taskList[taskNum - 1];
+
+            if (isDone) {
+                task.mark();
+                System.out.println(DIVIDER + " Nice! I've marked this task as done:");
+            } else {
+                task.unmark();
+                System.out.println(DIVIDER + " OK, I've marked this task as not done yet:");
+            }
+            System.out.println("   [" + task.getStatusIcon() + "] " + task.getDescription());
+            System.out.println(DIVIDER);
+
+        } catch (NumberFormatException e) {
+            System.out.println(DIVIDER + "Please provide a valid task number!\n" + DIVIDER);
+        }
+    }
+
     public static void main(String[] args) {
         greet();
         manageTask();
